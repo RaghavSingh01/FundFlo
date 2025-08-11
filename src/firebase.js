d8770e73ee77b43d0660b9b562e92d5629b22f3d
Raghav Singh <dhankarraghav13@gmail.com>
2025-08-11 20:48:40 +0530

    git ignore updated

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAafmV0D3w-Z8pASP6AeQ-4czwEJdsTA-M",
-  authDomain: "fundflo-8c935.firebaseapp.com",
-  projectId: "fundflo-8c935",
-  storageBucket: "fundflo-8c935.firebasestorage.app",
-  messagingSenderId: "14913520688",
-  appId: "1:14913520688:web:b0555e23ef64689493d9f5",
-  measurementId: "G-MP8K0E7MDL"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
